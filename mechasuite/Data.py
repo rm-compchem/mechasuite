3539d6d1f28d6b15e2fba8addcd15da8a2ba37d9
miguel <miroes1@itq.upv.es>
2026-08-05 13:09:12 +0200

    Added edit transmission coefficient function

--- mechasuite/Data.py
+++ mechasuite/Data.py
@@ -1054,6 +1054,7 @@ class Reac(object):
         self.abs_en = 0.0
         self.abs_zpe = 0.0
         self.zpe = 0.0
+        self.kappa = 1.0 # Default transmission coefficient
 
         self.thermo = {
             "g": {}, "h": {}, "stot": {},
@@ -1091,7 +1092,8 @@ class Reac(object):
         selfdict["ref"] = self.ref.name
         selfdict["name"] = self.name
         selfdict["thermo"] = self.thermo
-        selfdict["relref"] = self.relref
+        selfdict["relref"] = self.relref 
+        selfdict["kappa"] = self.kappa
         return selfdict
 
     def update(self):
@@ -1171,14 +1173,14 @@ class Reac(object):
                     self.thermo[key][T] = relp
 
             if self.parent_itm.tp == "ts":
-                self.thermo["prob"] = 1.0
+                self.thermo["prob"] = self.kappa
                 try:
-                    self.thermo["k"][T] = (Kb[u] * T / h[u]) * (np.e**(-self.thermo["g"][T] / (R[u] * T)))
+                    self.thermo["k"][T] = self.thermo["prob"] * (Kb[u] * T / h[u]) * (np.e**(-self.thermo["g"][T] / (R[u] * T)))
                 except Exception as e:
                     print("There was an error computing K", e)
                 try:
                     # No se porque hay un e multiplicando lo que deberia ser KbT/h * exp^R/T
-                    self.thermo["a"][T] = (np.e * Kb[u] * T / h[u])*(np.e**(self.thermo["stot"][T]/R[u])) 
+                    self.thermo["a"][T] = self.thermo["prob"] * (np.e * Kb[u] * T / h[u])*(np.e**(self.thermo["stot"][T]/R[u])) 
                 except Exception as e:
                     print("There was an error computing A", e)
                 try:
@@ -2915,6 +2917,7 @@ class Data(object):
                                 # add reac does not need a name, the name is taken
                                 # from the reference: reac obj
                                 itmobj.add_reac(reacobjs[0])
+                                reacobj = itmobj.get_reac(reacobjs[0].name)
                         else:
                             if "relref" in reac:
                                 # busco el object itm que es de referencia para itmobj
@@ -2923,6 +2926,11 @@ class Data(object):
                                     itmobj.add_reac(mecobj.get_itm(reac["ref"]), reac["relref"])
                                 except:
                                     pass
+                                else:
+                                    reacobj = itmobj.get_reac(reac["ref"])
+                        if reacobj is not None and "kappa" in reac:
+                            reacobj.kappa = float(reac["kappa"])
+                            reacobj.update()
                     if itm.get("states"):
                         for state in itm["states"]:
                             # NO PONGO LO DE "refs" PORQUE NO SE QUE ES
